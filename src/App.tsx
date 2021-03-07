f985e37397d9e3ff155aab70b1413661bb4ceeea
Avinash Iyer <avinashiyer4292@gmail.com>
2021-03-07 11:38:06 -0800

    Initial grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const numRows = 50;
+const numCols = 50;
+
+const App: React.FC = () => {
+  const [grid, setGrid] = useState(() => {
+    const rows = [];
+    for (let i = 0; i < numRows; i++) {
+      rows.push(Array.from(Array(numCols), () => 0));
+    }
+    console.log(rows);
+    return rows;
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid">
+      {grid.map((rows, i) =>
+        rows.map((cols, j) => (
+          <div
+            key={`${i}${j}`}
+            style={{
+              width: 20,
+              height: 20,
+              border: "1px solid black",
+              backgroundColor: grid[i][j] ? "red" : undefined,
+            }}
+          />
+        ))
+      )}
     </div>
   );
-}
+};
 
 export default App;
