275b345cd0db06010a5c3d35bdb78d4b44e72e35
Avinash Iyer <avinashiyer4292@gmail.com>
2021-03-08 00:35:51 -0800

    Basic game finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React, { useCallback, useState, useRef } from "react";
 import "./App.css";
+import produce from "immer";
 
-const numRows = 50;
-const numCols = 50;
+const numRows = 20;
+const numCols = 20;
 
 type directionType = {
   [key: string]: Array<number>;
@@ -14,15 +15,16 @@ const directions: directionType = {
   L: [0, -1],
 };
 
+const generateGrid = () => {
+  const rows = [];
+  for (let i = 0; i < numRows; i++) {
+    rows.push(Array.from(Array(numCols), () => 0));
+  }
+  return rows;
+};
+
 const App: React.FC = () => {
-  const [grid, setGrid] = useState(() => {
-    const rows = [];
-    for (let i = 0; i < numRows; i++) {
-      rows.push(Array.from(Array(numCols), () => 0));
-    }
-    console.log(rows);
-    return rows;
-  });
+  const [grid, setGrid] = useState(generateGrid);
 
   const [gameStarted, setGameStarted] = useState(false);
   const gameStartedRef = useRef(gameStarted);
@@ -32,18 +34,96 @@ const App: React.FC = () => {
   const directionRef = useRef(direction);
   directionRef.current = direction;
 
-  const [snakePositions, setSnakePositions] = useState([0, 0]);
+  const [snakePositions, setSnakePositions] = useState([[0, 0]]);
   const snakePositionsRef = useRef(snakePositions);
   snakePositionsRef.current = snakePositions;
 
+  const generateFoodPosition = () => {
+    let newFoodPositionX = Math.floor(Math.random() * numRows);
+    let newFoodPositionY = Math.floor(Math.random() * numCols);
+    return [newFoodPositionX, newFoodPositionY];
+  };
+
+  const [foodPosition, setFoodPosition] = useState(generateFoodPosition);
+  const foodPositionRef = useRef(foodPosition);
+  foodPositionRef.current = foodPosition;
+
+  const [isFoodPresent, setIsFoodPresent] = useState(false);
+  const isFoodPresentRef = useRef(isFoodPresent);
+  isFoodPresentRef.current = isFoodPresent;
+
+  const resetGame = () => {
+    setGrid(generateGrid);
+    setGameStarted(false);
+    setDirection("R");
+    setSnakePositions([[0, 0]]);
+  };
+
+  const setCellColor = (cellValue: number) => {
+    if (cellValue === 1) return "red";
+    if (cellValue === 2) return "blue";
+    return undefined;
+  };
+
+  const gameOver = () => {
+    alert("Game Over!!!");
+    resetGame();
+    return;
+  };
+
   const runGame = useCallback(() => {
     if (!gameStartedRef.current) return;
-    let newHeadX =
-      snakePositionsRef.current[0] + directions[directionRef.current][0];
-    let newHeadY =
-      snakePositionsRef.current[1] + directions[directionRef.current][1];
+    const newHeadX: number =
+      snakePositionsRef.current[0][0] + directions[directionRef.current][0];
+    const newHeadY: number =
+      snakePositionsRef.current[0][1] + directions[directionRef.current][1];
 
-    setTimeout(runGame, 1000);
+    //check for bounds
+    if (
+      newHeadX < 0 ||
+      newHeadX >= numRows ||
+      newHeadY < 0 ||
+      newHeadY >= numCols
+    )
+      gameOver();
+
+    //check if snake cuts itself
+    //if ()
+
+    let newPos = `${newHeadX},${newHeadY}`,
+      foodPos = `${foodPositionRef.current[0]},${foodPositionRef.current[1]}`;
+    console.log(`New position: ${newPos}`);
+    console.log(`Food position: ${foodPos}`);
+    let oldSnakePositions = snakePositionsRef.current;
+    if (newPos === foodPos) {
+      let newFoodPosition = generateFoodPosition();
+      setFoodPosition(newFoodPosition);
+      foodPositionRef.current = newFoodPosition;
+      console.log(
+        `Ate food! Now new Food position: ${newFoodPosition[0]},${newFoodPosition[1]}`
+      );
+    } else if (newPos !== foodPos) {
+      oldSnakePositions.pop();
+    }
+    oldSnakePositions.unshift([newHeadX, newHeadY]);
+    snakePositionsRef.current = oldSnakePositions;
+    // console.log(
+    //   `New snake positions: ${JSON.stringify(snakePositionsRef.current)}`
+    // );
+    setGrid((originalGrid) => {
+      return produce(originalGrid, (copiedGrid) => {
+        for (let i = 0; i < numRows; i++) {
+          for (let j = 0; j < numCols; j++) copiedGrid[i][j] = 0;
+        }
+        snakePositionsRef.current.forEach(([x, y]) => {
+          copiedGrid[x][y] = 1;
+        });
+        copiedGrid[foodPositionRef.current[0]][foodPositionRef.current[1]] = 2;
+        return copiedGrid;
+      });
+    });
+
+    setTimeout(runGame, 300);
   }, []);
 
   return (
@@ -51,20 +131,52 @@ const App: React.FC = () => {
       <button
         onClick={() => {
           setGameStarted(!gameStarted);
+          if (!gameStarted) {
+            gameStartedRef.current = true;
+            runGame();
+          }
         }}
       >
         Start Game
       </button>
+      <button onClick={() => {}}>Add to snake</button>
+      <button
+        onClick={() => {
+          resetGame();
+        }}
+      >
+        Stop
+      </button>
       <div className="controls-container">
         <div>
-          <button disabled={gameStarted && direction === "U"}>Up</button>
+          <button
+            onClick={() => setDirection("U")}
+            disabled={gameStarted && direction === "U"}
+          >
+            Up
+          </button>
         </div>
         <div>
-          <button disabled={gameStarted && direction === "L"}>Left</button>
-          <button disabled={gameStarted && direction === "R"}>Right</button>
+          <button
+            onClick={() => setDirection("L")}
+            disabled={gameStarted && direction === "L"}
+          >
+            Left
+          </button>
+          <button
+            onClick={() => setDirection("R")}
+            disabled={gameStarted && direction === "R"}
+          >
+            Right
+          </button>
         </div>
         <div>
-          <button disabled={gameStarted && direction === "D"}>Down</button>
+          <button
+            onClick={() => setDirection("D")}
+            disabled={gameStarted && direction === "D"}
+          >
+            Down
+          </button>
         </div>
       </div>
       <div
@@ -80,9 +192,9 @@ const App: React.FC = () => {
               style={{
                 width: 20,
                 height: 20,
-                border: "0.1px solid green",
+                border: "0.01px solid green",
                 margin: "2px",
-                backgroundColor: grid[i][j] ? "red" : undefined,
+                backgroundColor: setCellColor(grid[i][j]),
               }}
             />
           ))
